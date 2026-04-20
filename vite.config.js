f5f6d278adcef63fef5924cfa70d12740c0d7906
Naveen <naveenmalik0928@gmail.com>
2026-04-20 10:15:40 +0530

    update config

--- vite.config.js
+++ vite.config.js
@@ -8,8 +8,10 @@ export default defineConfig({
     chunkSizeWarningLimit: 1000,
     rollupOptions: {
       output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom', 'react-router-dom'],
+        manualChunks(id) {
+          if (id.includes('node_modules/react') || id.includes('node_modules/react-dom') || id.includes('node_modules/react-router')) {
+            return 'vendor';
+          }
         },
       },
     },
